0125787d79e29aa66f027dbba373b908731c1d97
Oleksandr Tereshchuk <atereshhuk68@ukr.net>
2022-06-13 15:03:18 +0300

    Fix. Refactor styles and js. Responsive

--- js/common.js
+++ js/common.js
@@ -1,44 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Close top banner
-	const closeBtn = document.querySelector(".top-banner__close");
-	const topBanner = document.querySelector(".top-banner");
+  // Close top banner
+  const closeBtn = document.querySelector(".top-banner__close");
+  const topBanner = document.querySelector(".top-banner");
 
-	closeBtn.addEventListener("click", () => {
-		topBanner.classList.add("top-banner--hidden");
-	});
+  closeBtn.addEventListener("click", () => {
+    topBanner.classList.add("top-banner--hidden");
+  });
 
-	// Move search to another place
-	const searchBar = document.querySelector(".sidebar-search");
-	const recentSection = document.querySelector(".recent");
-	const aside = document.querySelector(".aside");
+  // Move search to another place
+  const searchBar = document.querySelector(".sidebar-search");
+  const recentSection = document.querySelector(".recent");
+  const aside = document.querySelector(".aside");
 
-	function moveSearchBar () {
-		if (window.matchMedia("(max-width: 890px)").matches) {
-			recentSection.insertAdjacentElement("beforebegin", searchBar);
-		} else {
-			aside.insertAdjacentElement("afterbegin", searchBar);
-		}
-	}
+  function moveSearchBar () {
+    if (window.matchMedia("(max-width: 890px)").matches) {
+      recentSection.insertAdjacentElement("beforebegin", searchBar);
+    } else {
+      aside.insertAdjacentElement("afterbegin", searchBar);
+    }
+  }
 
-	// Move socieal icons to another place
-	const socialIcons = document.querySelector(".footer__social");
-	const footerColumn3 = document.querySelector(".footer__column-3");
-	const footerInfo = document.querySelector(".footer__info");
+  // Move socieal icons to another place
+  const socialIcons = document.querySelector(".footer__social");
+  const footerNavs = document.querySelector(".footer__navs ");
+  const footerInfo = document.querySelector(".footer__info");
 
-	function moveSocialIcons () {
-		if (window.matchMedia("(max-width: 880px)").matches) {
-			footerColumn3.insertAdjacentElement("beforeend", socialIcons);
-		} else {
-			footerInfo.insertAdjacentElement("beforeend", socialIcons);
-		}
-	}
+  function moveSocialIcons () {
+    if (window.matchMedia("(max-width: 600px)").matches) {
+      footerNavs.insertAdjacentElement("beforeend", socialIcons);
+    } else {
+      footerInfo.insertAdjacentElement("beforeend", socialIcons);
+    }
+  }
 
-	moveSocialIcons()
-	moveSearchBar();
+  moveSocialIcons();
+  moveSearchBar();
 
-	window.onresize = () => {
-		moveSocialIcons();
-		moveSearchBar();
-	}
+  window.onresize = () => {
+    moveSocialIcons();
+    moveSearchBar();
+  }
 
 })
